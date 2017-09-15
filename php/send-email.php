9f0984b842fb531af684b7be4010054c6c3f69c6
Igor Wilian Faoro <igor.faoro17@gmail.com>
2017-09-15 15:20:04 -0300

    Ajuste send-email.php

--- php/send-email.php
+++ php/send-email.php
@@ -1,12 +1,19 @@
 <?php 
 $name = isset($_POST['name']) ? $_POST['name'] : "Sem nome";
-$email = isset($_POST['email']) ? $_POST['email'] : "Sem email";
-$message = isset($_POST['message']) ? $_POST['message'] : "Sem message";
+$email = isset($_POST['email']) ? $_POST['email'] : "Sem e-mail";
+$message = isset($_POST['message']) ? $_POST['message'] : "Sem messagem";
 
-$sendTo = "empresa@empresa.com";
-$mailBody = "The text for the mail...";
-$subject = $message;
-$header = "From: ". $name . " <" . $email . ">\r\n";
+$sendTo = "digiletric@digiletric.com.br";
+$subject = "Mensagem de " . $name . " (www.digiletric.com.br)";
+
+$mailBody = "<b>E-mail:</b> " . $email . "<br>";
+$mailBody .= "<b>Nome:</b> " . $name . "<br>";
+$mailBody .= "<b>Data de envio:</b> " . date("d/m/Y") . "<br>";
+$mailBody .= "<b>Mensagem:</b><br>" . $message;
+
+$header = "Content-type: text/html";
 
 mail($sendTo, $subject, $mailBody, $header); 
+
+header('Location: ../index.html#contact');
 ?>
